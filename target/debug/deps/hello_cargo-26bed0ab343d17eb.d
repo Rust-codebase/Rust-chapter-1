37d82a6220dc846c1c1e53c865d9c93efc4a90f2
Aritra Dey <adey01027@gmail.com>
2024-08-24 03:36:00 +0530

    Added cargo basics

---
+++ target/debug/deps/hello_cargo-26bed0ab343d17eb.d
@@ -0,0 +1,5 @@
+/home/aritra/Downloads/projects/Rust-codebase/chap-1/hello_cargo/target/debug/deps/libhello_cargo-26bed0ab343d17eb.rmeta: src/main.rs
+
+/home/aritra/Downloads/projects/Rust-codebase/chap-1/hello_cargo/target/debug/deps/hello_cargo-26bed0ab343d17eb.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/aritra/Downloads/projects/Rust-codebase/chap-1/hello_cargo/target/debug/hello_cargo: /home/aritra/Downloads/projects/Rust-codebase/chap-1/hello_cargo/src/main.rs
